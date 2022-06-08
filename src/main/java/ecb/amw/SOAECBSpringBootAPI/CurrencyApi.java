34b39a30d7747fa7bfbbd20c543953a004d5ee2e
bchanowski <bartosz.chanowski@gmail.com>
2022-06-08 20:11:27 +0200

    Dodanie komunikatów przy złych danych wejściowych

--- src/main/java/ecb/amw/SOAECBSpringBootAPI/CurrencyApi.java
+++ src/main/java/ecb/amw/SOAECBSpringBootAPI/CurrencyApi.java
@@ -1,76 +1,91 @@
 package ecb.amw.SOAECBSpringBootAPI;
 
-
-import ecb.amw.SOAECBSpringBootAPI.SQLiteOperations;
-import ecb.amw.SOAECBSpringBootAPI.Currency;
 import org.springframework.web.bind.annotation.*;
 
+import java.text.SimpleDateFormat;
 import java.time.LocalDate;
 
-
 @RestController
-@RequestMapping("/api/currency/rates")
+@RequestMapping("/currency/rates")
 public class CurrencyApi extends SQLiteOperations {
 
-    public CurrencyApi()throws Exception{
+    public CurrencyApi() throws Exception {
 
     }
 
-    protected static Currency currencyToEuro(Currency currency){
-        double rate = 1/ currency.getRate();
-        Currency swap = new Currency(currency.getDate(), currency.getTo(), currency.getFrom(),rate);
+    protected static Currency currencyToEuro(Currency currency) {
+        Currency swap;
+        double rate = 1 / currency.getRate();
+        swap = new Currency(currency.getDate(), currency.getTo(), currency.getFrom(), rate);
         return swap;
     }
-    protected static Currency currencyToCurrency(Currency fromCurrency, Currency toCurrency){
+
+    protected static Currency currencyToCurrency(Currency fromCurrency, Currency toCurrency) {
         Currency swap;
-        double rate = toCurrency.getRate()/ fromCurrency.getRate();
+        double rate = toCurrency.getRate() / fromCurrency.getRate();
         swap = new Currency(fromCurrency.getDate(), fromCurrency.getTo(), toCurrency.getTo(), rate);
         return swap;
+    }
+
+    protected static boolean isValidDate(String date) {
+        SimpleDateFormat format = new SimpleDateFormat("yyyy-MM-dd");
+        format.setLenient(false);
+        try {
+            format.parse(date);
+        } catch (Exception e) {
+            return false;
+        }
+        return true;
+    }
+
+    private void checkCurrencies(@PathVariable String currencyFrom, @PathVariable String currencyTo, String userDate) {
+        if (currencyFrom.equals("EUR")) {
+            currency = readData(userDate, currencyTo);
+        } else if (currencyTo.equals("EUR")) {
+            currency = readData(userDate, currencyFrom);
+            currency = currencyToEuro(currency);
+        } else {
+            Currency exFrom = readData(userDate, currencyFrom);
+            Currency exTo = readData(userDate, currencyTo);
+            currency = currencyToCurrency(exFrom, exTo);
+        }
     }
 
     LocalDate today = LocalDate.now();
     String string2Date = today.toString();
     String userDate = today.toString();
+    Currency currency = new Currency();
     String message = "";
 
     @GetMapping("{currencyFrom}/{currencyTo}")
     @ResponseBody
-    public String endPoint(@PathVariable String currencyFrom, @PathVariable String currencyTo){
-
-        Currency currency = new Currency();
-        if(currencyFrom.equals("EUR")) {
-            currency = readData(string2Date, currencyTo);
-        } else if(currencyTo.equals("EUR")){
-            currency = readData(string2Date,currencyFrom);
-            currency = currencyToEuro(currency);
-        }else {
-            Currency exFrom = readData(string2Date,currencyFrom);
-            Currency exTo = readData(string2Date,currencyTo);
-            currency = currencyToCurrency(exFrom,exTo);
+    public String endPoint(@PathVariable String currencyFrom, @PathVariable String currencyTo) {
+        checkCurrencies(currencyFrom, currencyTo, string2Date);
+        if (currency.getRate() == 0 || Double.isNaN(currency.getRate())) {
+            message = "Currencies not available in our service!";
+        } else {
+            message = "Current exchange rate from " + currencyFrom + " to " + currencyTo + " is: " + currency.getRate();
         }
-        message = "Current exchange rate from " + currencyFrom + " to " + currencyTo + " is: " + currency.getRate();
         return message;
     }
 
-    @GetMapping("{year}/{month}/{day}/{currencyFrom}/{currencyTo}")
+    @GetMapping("{currencyFrom}/{currencyTo}/{year}/{month}/{day}")
     @ResponseBody
-    public String endPointHistorical(@PathVariable String year, @PathVariable String month, @PathVariable String day, @PathVariable String currencyFrom, @PathVariable String currencyTo){
+    public String endPointHistorical(@PathVariable String year, @PathVariable String month, @PathVariable String day,
+            @PathVariable String currencyFrom, @PathVariable String currencyTo) {
 
         userDate = year + "-" + month + "-" + day;
-        Currency currency = new Currency();
-        if(currencyFrom.equals("EUR")) {
-            currency = readData(userDate, currencyTo);
-        } else if(currencyTo.equals("EUR")){
-            currency = readData(userDate,currencyFrom);
-            currency = currencyToEuro(currency);
-        }else {
-            Currency exFrom = readData(userDate,currencyFrom);
-            Currency exTo = readData(userDate,currencyTo);
-            currency = currencyToCurrency(exFrom,exTo);
+        if(!isValidDate(userDate)){
+            message = "Not a proper date!";
+        } else {
+            checkCurrencies(currencyFrom, currencyTo, userDate);
+            if (currency.getRate() == 0 || Double.isNaN(currency.getRate())) {
+                message = "Currencies or date not available in our service!";
+            } else {
+                message = "Exchange rate for date: " + userDate + " from " + currencyFrom + " to " + currencyTo + " is: " + currency.getRate();
+            }
         }
-        message = "Currency rate for date: " + userDate + " from " + currencyFrom + " to " + currencyTo + " is: " + currency.getRate();
         return message;
     }
-
 
 }
